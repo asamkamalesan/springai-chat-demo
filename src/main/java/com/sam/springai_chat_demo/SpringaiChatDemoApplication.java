d240469465176c1bb75783c9fdd6bf59dc28f766
asamkamalesan <asamkamalesan@gmail.com>
2026-07-09 07:15:28 +0530

    initial commit

---
+++ src/main/java/com/sam/springai_chat_demo/SpringaiChatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sam.springai_chat_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiChatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiChatDemoApplication.class, args);
+	}
+
+}
